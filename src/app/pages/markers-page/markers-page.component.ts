872ff5389a1fe0071c2b2a092e3dc740fd6eb95b
Fernando Herrera <fernando.herrera85@gmail.com>
2025-01-15 11:28:13 -0500

    add Mapbox integration with fullscreen map component and controls

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -5,4 +5,4 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './markers-page.component.html',
 })
-export class MarkersPageComponent { }
+export class MarkersPageComponent {}
